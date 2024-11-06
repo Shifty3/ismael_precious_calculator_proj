b71bd43cdc2652a63fb9cbc0f7cdd7d817f5111f
Ismael Martinez <Ismael@Administrators-MacBook-Pro.local>
2024-11-05 20:35:54 -0500

    changes to html css and js

---
+++ script.js
@@ -0,0 +1,29 @@
+//first we want to take the value of each key 
+let ac = document.querySelector(".ac").innerHTML;
+let negative = document.querySelector(".negative").innerHTML;
+let percent = document.querySelector(".percent").innerHTML;
+let divide = document.querySelector(".divide").innerHTML;
+let seven = parseInt(document.querySelector(".seven").innerHTML);
+let eight = parseInt(document.querySelector(".eight").innerHTML);
+let nine = parseInt(document.querySelector(".nine").innerHTML);
+let multiply = document.querySelector(".multiply").innerHTML;
+let four = parseInt(document.querySelector(".four").innerHTML);
+let five = parseInt(document.querySelector(".five").innerHTML);
+let six = parseInt(document.querySelector(".six").innerHTML);
+let minus = document.querySelector(".minus").innerHTML;
+let one = parseInt(document.querySelector(".one").innerHTML);
+let two = parseInt(document.querySelector(".two").innerHTML);
+let three = parseInt(document.querySelector(".three").innerHTML);
+let plus = document.querySelector(".plus").innerHTML;
+let backspace = document.querySelector(".backspace").innerHTML;
+let zero = parseInt(document.querySelector(".zero").innerHTML);
+let dot = document.querySelector(".dot").innerHTML;
+let equal = document.querySelector(".equal").innerHTML;
+//then we diplay it on the output screen
+//after we makee it so that user can input any equation
+//then make sure '=' sums up the equation 
+//next we implement the clear button to work
+  
+
+
+
