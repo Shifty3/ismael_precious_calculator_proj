a70110ce137658fc24b8b216b2a4800a6fef0a6c
preciousdsmith <precious.d.smith@gmail.com>
2024-11-08 14:07:13 -0500

    new update

--- script.js
+++ script.js
@@ -25,127 +25,45 @@
 //refactored code !!!
 //null is so we assisn the var with no value
 let output = document.querySelector(".output-field");
-let firstVal = null;
-let secondVal = null;
-let operatorVal = null;
-let result = null;
+var firstVal 
+var secondVal 
+var operatorVal;
+var result;
+//output values
+let numArray1 = [];
+let numArray2 = [];
 
 //handles button clicks
 function buttonClick(btnVal) {
-  //append the clicked value to the current display as a string
-  output.innerHTML += btnVal;
+  numArray1.push(btnVal);
+  //makes array one number
+  const numString = numArray1.join("");
+  console.log(numString);
+  output.innerHTML = numString;
 }
 
-//function to handle button operator clicks
-function operatorBtn(operator){
-  if (operator === "AC"){
-    clearDisplay();
-  } else if (operator === "+/-"){
-    toggleNegative();
-} else if (operator === "%"){
-  calculatePercent();
-} else if (operator === "<~"){
-  backspace();
-} else if (operator === "="){
-  calculateResult();
-} else {
-  //stores first value and operator btn so that it knows what operator to use
-  storeFirstValue(operator);
-}
-}
-
-//resets values to default
-function clearDisplay() {
-   output.innerHTML = "";
-   var firstVal = null;
-   var secondVal = null;
-   var operatorVal = null;
-   var result = null;
-}
-
-function toggleNegative(){
-  let currentValue = parseFloat(output.innerHTML);
-  if (!isNaN(currentValue)){
-    output.innerHTML = currentValue * -1;
+//storing the expression
+function operatorBtn(operatoerSign) {
+  output.innerHTML = "";
+  //when i click + its goig to add
+  console.log(operatoerSign);
+  console.log("we are doing ____ operation");
+  if (operatoerSign === "+") {
+    operatorVal = operatoerSign;
   }
-}
 
-function calculatePercent(){
-  let currentValue = parseFloat(output.innerHTML);
-  //checking if currentValue is a number
-  if (!isNaN(currentValue)){
-    output.innerHTML = currentValue / 100;
-  }
-}
-
-function backspace(){
-  //removes last character from display
-  output.innerHTML = output.innerHTML.slice(0,-1);
-}
-
-function storeFirstValue(operator){
-  //stores first value
-  firstVal = parseFloat(output.innerHTML);
-  //stores operatopr
-  operatorVal = operator;
-  //clear display after second number
-  output.innerHTML = ""
-}
-
-function calculateResult(){
-  //stores second value
-  secondVal = parseFloat(output.innerHTML);
-  if (firstVal !== null && secondVal !== null && operatorVal !== null){
-    if (operatorVal === "+"){
-      result = firstVal + secondVal;
-    } 
-    else if (operatorVal === "-"){
-      result = firstVal - secondVal;
-    } 
-    else if (operatorVal === "x"){
-      result = firstVal * secondVal;
-    } 
-    else if (operatorVal !== 0){
-      result = firstVal / secondVal;
-    } 
-    else {
-      result = "Error";//advoid numbers divided by zero
+  if (operatoerSign === "=") {
+    if (operatorVal === "+") {
+      runAddOperation();
+      output.innerHTML = result;
     }
   }
-
-  output.innerHTML = result;
-  firstVal = result;
-  operatorVal = null;
 }
 
-
-
-
-
-
-
-// //storing the expression
-// function operatorBtn(operatoerSign) {
-//   output.innerHTML = "";
-//   //when i click + its goig to add
-//   console.log(operatoerSign);
-//   console.log("we are doing ____ operation");
-//   if (operatoerSign === "+") {
-//     operatorVal = operatoerSign;
-//   }
-
-//   if (operatoerSign === "=") {
-//     if (operatorVal === "+") {
-//       runAddOperation();
-//       output.innerHTML = result;
-//     }
-//   }
-// }
-
-// //adding event handlers for each buttons
-// function runAddOperation() {
-//   result = firstVal + secondVal;
-// }
+//adding event handlers for each buttons
+function runAddOperation() {
+  result = firstVal + secondVal;
+}
 //then we diplay each value on the output screen
 
 //when i click on +/- its going to make the number a negative
